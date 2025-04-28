babad7c89d60878d307d80718632a6f9a617d89f
Sergiy <sergiykochenko@gmail.com>
2025-04-28 22:36:59 +0100

    Socket Implementation

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -10,6 +10,7 @@ const Message = ({message}) => {
 	const chatClassName = fromMe ? 'chat-end' : 'chat-start';
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const buubbleBgColor = fromMe ? 'bg-blue-500' : '';
+	const shackClass = message.shouldShake ? 'shake' : '';
 	return (
 		<div className= {`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
@@ -19,7 +20,7 @@ const Message = ({message}) => {
 					/>
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${buubbleBgColor} pb-2`}>{message.message}</div>
+			<div className={`chat-bubble text-white ${buubbleBgColor} ${shackClass} pb-2`}>{message.message}</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formatedTime}</div>
 		</div>
 	);
